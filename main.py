c5927ffaa8949fa70fce79296afd1fde5b63d64c
Ezekiel Ramirez <ezekiel.ramirez@calbaptist.edu>
2022-07-09 13:44:28 -0700

    Hopefully my code is imported

---
+++ main.py
@@ -0,0 +1,20 @@
+# print("Hello World")
+# this hashtag mark makes a comment
+
+print("/___|")
+print("   /|")
+print("  / |")
+# print()
+print(" /  |")
+
+print()
+
+character_name = "Zeek"
+character_age = "21"
+another_age = 41
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age)
+# print("I am printing " + another_age + " which is an int rather than a string")
