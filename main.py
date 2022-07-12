2f4d2d14b867aa78d8871be44196610e70ac9be2
Ezekiel Ramirez <ezekiel.ramirez@calbaptist.edu>
2022-07-12 13:18:29 -0700

    Added 20 more minutes of practice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # print("Hello World")
 # this hashtag mark makes a comment
 
+"""
 print("/___|")
 print("   /|")
 print("  / |")
@@ -11,10 +12,38 @@ print()
 
 character_name = "Zeek"
 character_age = "21"
-another_age = 41
 
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age + " years old.")
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age)
-# print("I am printing " + another_age + " which is an int rather than a string")
+
+another_age = 41
+# to print a number as a string try casting it to string format
+print("I am printing " + str(another_age) + " which is an int rather than a string")
+
+is_male = True
+"""
+
+# docstrings -> """ can create blocks of multi-line comments
+
+"""
+
+print("Giraffe\nAcademy")
+phrase = "Zeek's Study Time"
+print(phrase + "\nis well worth the effort\n")
+
+print(phrase.lower() + "\t\t" + phrase.upper())
+
+print(len(phrase))
+print(phrase[0] + phrase[7] + phrase[13])
+# this following print stmt is returning the
+# location where the apostrophe is located in the string
+print(phrase.index("'"))
+print(phrase.index("Ti"))
+
+# string.replace function
+print("\n" + phrase.replace("Zeek's", "Cameron's"))
+"""
+
+# numbers
